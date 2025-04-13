6d321f2a040aecbba9f491c29f9a7ae63ad6e8fd
Khoa Van <van.kh@northeastern.edu>
2025-04-13 07:25:47 -0400

    Update environment

--- src/train.py
+++ src/train.py
@@ -1,23 +1,22 @@
 import os
 import torch
-from transformer import (
+from transformers import (
     AutoModelForCausalLM, 
     AutoTokenizer, 
     Trainer, 
     TrainingArguments, 
-    DataCollatorForLanguageModelling
+    DataCollatorForLanguageModeling
     )
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from bitsandbytes.config import BitsAndBytesConfig
 
 #Configs
-model_name = 'mistralai/Mistral-7B-v0.1'
+model_name = 'microsoft/phi-2'
 dataset_path = 'data/train.jsonl'
 output_dir = './output'
 
 #Load tokenizer and dataset
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 dataset = load_dataset('json', data_files={'train': dataset_path}['train'])
 
@@ -40,27 +39,21 @@ def tokenize(example):
 
 tokenized_dataset = dataset.map(tokenize, batched=True)
 
-#Load model with QLoRA
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type='nf4',
-    bnb_4bit_compute_dtype=torch.float32,
-    bnb_4bit_use_double_quant=True
-)
+#Load model with LoRA
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    quantization_config=bnb_config,
-    device_map='auto',
-    trust_remote_code=True
+    torch_dtype=torch.float32
 )
 
 model = prepare_model_for_kbit_training(model)
 
 lora_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    target_modules=['q_proj','k_proj', 'v_proj', 'o_proj']
+    r=8,
+    lora_alpha=16,
+    target_modules=['q_proj','k_proj', 'v_proj', 'o_proj'],
     lora_dropout=0.05,
     bias='none',
     task_type='CAUSAL_LM'
@@ -72,12 +65,11 @@ model = get_peft_model(model, lora_config)
 training_args = TrainingArguments(
     output_dir=output_dir,
     per_device_train_batch_size=1,
-    gradient_accumulation_steps=16,
-    num_train_epochs=3,
+    gradient_accumulation_steps=8,
+    num_train_epochs=2,
     learning_rate=2e-4,
-    fp16=False,
-    logging_dir=f'{output_dir}/logs'
-    save_total_limit=2,
+    logging_dir=f'{output_dir}/logs',
+    save_total_limit=1,
     save_strategy='epoch',
     logging_steps=10,
     disable_tqdm=False,
@@ -85,7 +77,7 @@ training_args = TrainingArguments(
     report_to='none'
 )
 
-data_collator = DataCollatorForLanguageModelling(tokenizer=tokenizer, mlm=False)
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 trainer = Trainer(
     model=model,
     args=training_args,
